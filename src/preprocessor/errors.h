6ab00bc80cb9dc307c58fc9a6189f89630d50e3a
Quentin-Clement <quentin.clement@algosup.com>
2024-02-12 09:28:25 +0100

    feat: created errors.h

---
+++ src/preprocessor/errors.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define byte unsigned char
+#define u64 unsigned long long
+#define i64 long long
+#define f64 double
+
+void file_not_found(char *filename)
+{
+    scanf("%s", str);
+}
