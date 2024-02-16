301fbfdb5b155c7fa27ab5b758cda6b3451ea35d
Malo-Archimbaud <malo.archimbaud@algosup.com>
2024-02-16 14:21:30 +0100

    Can read from a binary file
    Main can read from a file with a binary code assembled for our cpu and execute it

--- src/main.c
+++ src/main.c
@@ -1,29 +1,46 @@
 #include "cpu.h"
+#include <ctype.h>
+
+#define MAXMEM 1024
+#define BITS 32
 
 int main(int argc, char const *argv[])
 {
-    argv[1] = "out.bin";
-    // if (argc < 2)
-    // {
-        // printf("Usage: %s <file>\n", argv[0]);
-        // return 1;
-    // }
+    if (argc < 2)
+    {
+        printf("Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
     FILE *file = fopen(argv[1], "rb");
     if (file == NULL)
     {
         printf("Error: File not found\n");
         return 1;
     }
-    int i;
-    uint mem[10];
-    for (i = 0; i < 10; i++)
+
+    char line[BITS + 1];
+    uint mem[MAXMEM];
+    int count = 0;
+
+    for (int i = 0; i < MAXMEM; i++)
     {
-        fread(&mem[i], sizeof(uint), 1, file);
+        mem[i] = 0;
     }
+
+    while (fgets(line, sizeof(line), file)) {
+        mem[count++] = (uint)strtoul(line, NULL, 2);
+        if (count >= MAXMEM) {
+            printf("Maximum number of numbers reached\n");
+            break;
+        }
+    }
+
     fclose(file);
-    cpu_t *cpu = new_cpu(mem, 10);
+
+    cpu_t *cpu = new_cpu(mem, count);
     run_cpu(cpu);
     printf("Result: %d\n", cpu->r[1]);
     free_cpu(cpu);
+
     return 0;
 }
